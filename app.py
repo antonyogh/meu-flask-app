e00ab0517d273e17d3ced03e85196468df91cfa1
antonyogh <antoniocunha2805@gmail.com>
2025-08-09 20:24:23 -0300

    api-flask

--- app.py
+++ app.py
@@ -2,17 +2,26 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-servicos = [{'id':1,'servico':'Troca de óleo'},
-            {'id':2,'servico':'Troca de pastihas'},
-            {'id':3,'servico':'Troca de correia dentada'}]
+servicos = [
+    {'id':1,'servico':'Troca de óleo'},
+    {'id':2,'servico':'Troca de pastihas'},
+    {'id':3,'servico':'Troca de correia dentada'},
+]
 
 @app.route("/")
 def home():
     return jsonify({'status':'Api inicializada','versão':'1.0'})
 
-@app.route('/servicos',methods=['GET'])
-def servicos():
-    return jsonify({'servicos':servicos})
+@app.route('/servicos',methods=["GET"])
+def get_servicos():
+    return jsonify(servicos)
+
+@app.route('/servicos/<int:id>',methods=['GET'])
+def get_servico_id(id:int):
+    for s in servicos:
+        if s['id']==id:
+            return jsonify(s)
+    return jsonify({'message':'Serviço não encontrado!'})
 
 @app.route('/add',methods=['POST'])
 def add():
@@ -24,5 +33,6 @@ def add():
     return jsonify({'message':'Servico inserirdo!','servico':s}),201
 
 if __name__ == "__main__":
+    print(servicos)
     app.run()
     
