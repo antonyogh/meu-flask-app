1b22bba840e8a383b74cfdb8d4d3a23c5c2461b2
antonyogh <antoniocunha2805@gmail.com>
2025-08-09 00:28:47 -0300

    api-flask

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+servicos = [{'id':1,'servico':'Troca de óleo'},
+            {'id':2,'servico':'Troca de pastihas'},
+            {'id':3,'servico':'Troca de correia dentada'}]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return jsonify({'status':'Api inicializada','versão':'1.0'})
 
-@app.route('/sobre')
-def sobre():
-    return 'Desenvolvido por Antonio'
+@app.route('/servicos',methods=['GET'])
+def servicos():
+    return jsonify({'servicos':servicos})
+
+@app.route('/add',methods=['POST'])
+def add():
+    if not request.json or not 'id' in request.json or not 'servico' in request.json:
+        return jsonify({'message':'Dados inválidos!'}),400
+    
+    s = {'id': request.json['id'],'servico':request.json['servico']}
+    servicos.append(s)
+    return jsonify({'message':'Servico inserirdo!','servico':s}),201
 
 if __name__ == "__main__":
     app.run()
