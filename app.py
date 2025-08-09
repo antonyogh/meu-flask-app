1801dface57d3ececfca001c6254bba551d6278e
antonyogh <antoniocunha2805@gmail.com>
2025-08-08 21:24:21 -0300

    Alter

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/sobre')
+def sobre():
+    return 'Desenvolvido por Antonio'
+
 if __name__ == "__main__":
     app.run(debug=True)
